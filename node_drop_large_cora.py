b1e20847dd81422976d4659abfb1f48c8c8d66f3
Mahdi-Razi-Gandomani <mahdi.razi099@gmail.com>
2025-01-25 02:13:16 +0330

    Update node_drop_large.py

--- node_drop_large_cora.py
+++ node_drop_large_cora.py
@@ -12,33 +12,19 @@ import pickle
 import collections
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch_geometric.datasets import Planetoid
-from torch_geometric.utils import add_self_loops, degree
-from sklearn.metrics import accuracy_score
-import random
-from torch_geometric.transforms import RootedEgoNets
-from torch_geometric.utils import k_hop_subgraph
-import torch_geometric
-import time
-from itertools import chain, combinations
-import numpy as np
+from torch_geometric.datasets import Planetoid, WikiCS
 import pickle
 import pandas as pd
-import argparse
 import torch.nn.functional as F
 import collections
-import copy
 from torch_geometric.nn import SGConv
-import os 
 import torch_geometric.transforms as T
 import os
 import re
 from torch_geometric.data import Data
-from torch_geometric.datasets import Amazon
+from tqdm import tqdm
 import warnings
 warnings.simplefilter(action='ignore', category=Warning)
-import itertools
 
 class SGCNet(nn.Module):
     """
@@ -78,7 +64,7 @@ class SGCNet(nn.Module):
         _, pred = model(input_data).max(dim=1)
         correct = float (pred[input_data.test_mask].eq(input_data.y[input_data.test_mask]).sum().item())
         acc = correct / input_data.test_mask.sum().item()
-        print('Test Accuracy: {:.4f}'.format(acc))
+        # print('Test Accuracy: {:.4f}'.format(acc))
         return acc
 
     def predict_valid(self, dataset):
@@ -89,7 +75,7 @@ class SGCNet(nn.Module):
         _, pred = model(input_data).max(dim=1)
         correct = float (pred[input_data.val_mask].eq(input_data.y[input_data.val_mask]).sum().item())
         acc = correct / input_data.val_mask.sum().item()
-        print('Validation Accuracy: {:.6f}'.format(acc))
+        # print('Validation Accuracy: {:.6f}'.format(acc))
         return acc
 
     
@@ -115,7 +101,7 @@ def set_masks_from_indices(data, indices_dict, device):
 
 def get_subgraph_data(data, mask):
     """Extract subgraph data based on the given mask"""
-    nodes = mask.nonzero().view(-1)
+    nodes = mask.nonzero().reshape(-1)
     edge_mask_src = (data.edge_index[0].unsqueeze(-1) == nodes.unsqueeze(0)).any(dim=-1)
     edge_mask_dst = (data.edge_index[1].unsqueeze(-1) == nodes.unsqueeze(0)).any(dim=-1)
     edge_mask = edge_mask_src & edge_mask_dst
@@ -127,134 +113,149 @@ def get_subgraph_data(data, mask):
     sub_data = Data(x=data.x, edge_index=sub_edge_index, y=data.y, test_mask=test_mask, val_mask=val_mask)
     return sub_data
 
-# Experimental parameters
-group_trunc_ratio_hop_1 = 0.5
-group_trunc_ratio_hop_2 = 0.7
-ratio = 3
-directory = 'value/'
-pattern = re.compile(r'^Cora_(\d+)_10_0_0\.5_0\.7_pc_value\.pkl$')
+if __name__ == '__main__':
+    # Experimental parameters
+    dataset_name = 'Cora'
+    num_perm = 10
+    group_trunc_ratio_hop_1 = 0.5
+    group_trunc_ratio_hop_2 = 0.7
 
-# Find matching files for PC-Winter results
-matching_files = []
-for filename in os.listdir(directory):
-    if pattern.match(filename):
-        matching_files.append(filename)
-filenames = matching_files[:ratio]
-
-# Extract and aggregate PC-Winter values
-results = collections.defaultdict(list)
-counter = 0 
-for filename in filenames:
-    with open('value/' + filename, 'rb') as f:
-        data = pickle.load(f)
-    for key, sub_dict in data.items():
-        for sub_key, sub_sub_dict in sub_dict.items():
-            for sub_sub_key, value in sub_sub_dict.items():
-                results[(key, sub_key, sub_sub_key)].append(value)
-    counter += 1
-
-# Average the values
-for key, values in results.items():
-    results[key] = sum(values) / (len(values)*10)
-
-# Convert to DataFrame
-data = [{'key1': k1, 'key2': k2, 'key3': k3, 'value': v} for (k1, k2, k3), v in results.items()]
-win_df = pd.DataFrame(data)
-
-# Aggregate values for different hop levels
-win_df_11 = pd.DataFrame(win_df [win_df['key2'].isin(win_df['key1']) == False] .groupby('key2').value.sum().sort_values()).reset_index()
-win_df_11.columns= ['key', 'value']
-hop_1_list = win_df [win_df['key2'].isin(win_df['key1']) == False]['key2'].unique()
-win_df_12 = pd.DataFrame(win_df [(win_df['key3'] != win_df['key2'])&(win_df['key3'].isin(hop_1_list) )].groupby('key3').value.sum().sort_values()).reset_index()
-win_df_12.columns= ['key', 'value']
-
-win_df_1 =  pd.DataFrame(pd.concat([win_df_11, win_df_12]).groupby('key').value.sum().sort_values()).reset_index()
-win_df_2 = pd.DataFrame(win_df [(win_df['key3'].isin(win_df['key2']) == False)&(win_df['key3'].isin(win_df['key1']) == False)] .groupby('key3').value.sum().sort_values()).reset_index()
-win_df_2.columns= ['key', 'value']
-
-# Combine and sort unlabeled nodes
-unlabled_win_df = pd.concat([win_df_1,win_df_2])
-unlabled_win_df = unlabled_win_df.sort_values('value',ascending= False)
-unlabeled_win = torch.tensor(unlabled_win_df['key'].values)
-unlabeled_win_value = unlabled_win_df['value'].values
-
-# Load and preprocess the Cora dataset
-dataset = Planetoid(root='dataset/', name='Cora', transform=T.NormalizeFeatures())
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-data = dataset[0].to(device)
-
-train_mask = data.train_mask
-val_mask = data.val_mask 
-test_mask = data.test_mask 
-
-# Print dataset sizes
-print(f"Training size: {train_mask.sum().item()}")
-print(f"Validation size: {val_mask.sum().item()}")
-print(f"Test size: {test_mask.sum().item()}")
-
-# Create inductive edge index (removing edges to val/test nodes)
-inductive_edge_index = []
-for src, tgt in data.edge_index.t().tolist():
-    if not (val_mask[src] or test_mask[src] or val_mask[tgt] or test_mask[tgt]):
-        inductive_edge_index.append([src, tgt])
-inductive_edge_index = torch.tensor(inductive_edge_index).t().contiguous()
-
-indu_mask = torch.ones(data.edge_index.shape[1], dtype=torch.bool)
-for i, (src, tgt) in enumerate(data.edge_index.t().tolist()):
-    if val_mask[src] or test_mask[src] or val_mask[tgt] or test_mask[tgt]:
-        indu_mask[i] = False
-indu_mask  = indu_mask.to(device)
-
-# Prepare test and validation data
-test_data = get_subgraph_data(data, data.test_mask)
-val_data = get_subgraph_data(data, data.val_mask)
-
-# Node dropping experiment
-win_acc = []
-val_acc_list = []
-node_list = unlabeled_win.cpu().numpy()
-drop_num = len(node_list)+1
-
-# Initial model training and evaluation
-data_copy = data.clone()
-data_copy = data_copy.to(device)
-data_copy.edge_index = data_copy.edge_index[:,  indu_mask]
-
-model = SGCNet(num_features=dataset.num_features, num_classes=dataset.num_classes).to(device)
-model.fit(data_copy)
-test_acc = model.predict(test_data)
-val_acc = model.predict_valid(val_data )
-win_acc  +=[test_acc]
-val_acc_list += [val_acc]
-
-# Iteratively drop nodes and evaluate
-for j in range(1,drop_num):
-    cur_player = node_list[j-1]
-    print('cur_player: ',cur_player)
-    cur_node_list = node_list[:j]
-
-    edge_mask = torch.ones(data.edge_index.shape[1], dtype=torch.bool, device=device)
-    for node in cur_node_list:
-        edge_mask[data.edge_index[0] == node] = False
-        edge_mask[data.edge_index[1] == node] = False
-
-    edge_mask = edge_mask & indu_mask
+    directory = 'value/'
+    pattern = re.compile(
+    rf'^{dataset_name}_(\d+)_{num_perm}_0_{group_trunc_ratio_hop_1}_{group_trunc_ratio_hop_2}_pc_value\.pkl$'
+)
+    # Find matching files for PC-Winter results
+    ratio = 3
+    matching_files = []
+    for filename in os.listdir(directory):
+        if pattern.match(filename):
+            matching_files.append(filename)
+    filenames = matching_files[:ratio]
+    
+    # Extract and aggregate PC-Winter values
+    results = collections.defaultdict(list)
+    counter = 0 
+    for filename in filenames:
+        with open('value/' + filename, 'rb') as f:
+            data = pickle.load(f)
+        for key, sub_dict in data.items():
+            for sub_key, sub_sub_dict in sub_dict.items():
+                for sub_sub_key, value in sub_sub_dict.items():
+                    results[(key, sub_key, sub_sub_key)].append(value)
+        counter += 1
+    
+    # Average the values
+    for key, values in results.items():
+        results[key] = sum(values) / (len(values) * num_perm)
+    
+    # Convert to DataFrame
+    data = [{'key1': k1, 'key2': k2, 'key3': k3, 'value': v} for (k1, k2, k3), v in results.items()]
+    win_df = pd.DataFrame(data)
+    
+    # Aggregate values for different hop levels
+    win_df_11 = pd.DataFrame(win_df [win_df['key2'].isin(win_df['key1']) == False] .groupby('key2').value.sum().sort_values()).reset_index()
+    win_df_11.columns= ['key', 'value']
+    hop_1_list = win_df [win_df['key2'].isin(win_df['key1']) == False]['key2'].unique()
+    win_df_12 = pd.DataFrame(win_df [(win_df['key3'] != win_df['key2'])&(win_df['key3'].isin(hop_1_list) )].groupby('key3').value.sum().sort_values()).reset_index()
+    win_df_12.columns= ['key', 'value']
+    
+    win_df_1 =  pd.DataFrame(pd.concat([win_df_11, win_df_12]).groupby('key').value.sum().sort_values()).reset_index()
+    win_df_2 = pd.DataFrame(win_df [(win_df['key3'].isin(win_df['key2']) == False)&(win_df['key3'].isin(win_df['key1']) == False)] .groupby('key3').value.sum().sort_values()).reset_index()
+    win_df_2.columns= ['key', 'value']
+    
+    # Combine and sort unlabeled nodes
+    unlabled_win_df = pd.concat([win_df_1,win_df_2])
+    unlabled_win_df = unlabled_win_df.sort_values('value',ascending= False)
+    unlabeled_win = torch.tensor(unlabled_win_df['key'].values)
+    unlabeled_win_value = unlabled_win_df['value'].values
+    
+    # Load and preprocess the dataset
+    if dataset_name == 'WikiCS':
+        dataset = WikiCS(root='dataset/' + dataset_name, transform=T.NormalizeFeatures())
+    else:
+        dataset = Planetoid(root='dataset/' + dataset_name, name=dataset_name, transform=T.NormalizeFeatures())
+        
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    data = dataset[0].to(device)
+    
+    if dataset_name == 'WikiCS':
+        train_mask = data.train_mask[:, 0]
+        val_mask = data.val_mask[:, 0]
+        test_mask = data.test_mask
+       
+    else: 
+        train_mask = data.train_mask
+        val_mask = data.val_mask
+        test_mask = data.test_mask
+    
+    # Print dataset sizes
+    print(f"Training size: {train_mask.sum().item()}")
+    print(f"Validation size: {val_mask.sum().item()}")
+    print(f"Test size: {test_mask.sum().item()}")
+    
+    # Create inductive edge index (removing edges to val/test nodes)
+    inductive_edge_index = []
+    for src, tgt in data.edge_index.t().tolist():
+        if not (val_mask[src] or test_mask[src] or val_mask[tgt] or test_mask[tgt]):
+            inductive_edge_index.append([src, tgt])
+    inductive_edge_index = torch.tensor(inductive_edge_index).t().contiguous()
+    
+    indu_mask = torch.ones(data.edge_index.shape[1], dtype=torch.bool)
+    for i, (src, tgt) in enumerate(data.edge_index.t().tolist()):
+        if val_mask[src] or test_mask[src] or val_mask[tgt] or test_mask[tgt]:
+            indu_mask[i] = False
+    indu_mask  = indu_mask.to(device)
+    
+    # Prepare test and validation data
+    test_data = get_subgraph_data(data, data.test_mask)
+    val_data = get_subgraph_data(data, data.val_mask)
+    
+    # Node dropping experiment
+    win_acc = []
+    val_acc_list = []
+    node_list = unlabeled_win.cpu().numpy()
+    drop_num = len(node_list)+1
+    
+    # Initial model training and evaluation
     data_copy = data.clone()
     data_copy = data_copy.to(device)
-    data_copy.edge_index = data_copy.edge_index[:, edge_mask]
-
+    data_copy.edge_index = data_copy.edge_index[:,  indu_mask]
+    
     model = SGCNet(num_features=dataset.num_features, num_classes=dataset.num_classes).to(device)
     model.fit(data_copy)
     test_acc = model.predict(test_data)
     val_acc = model.predict_valid(val_data )
     win_acc  +=[test_acc]
     val_acc_list += [val_acc]
-        
-# Save results
-path = 'res/'
-os.makedirs(path, exist_ok=True)
-with open(os.path.join(path, f'node_drop_large_winter_value_{group_trunc_ratio_hop_1}_{group_trunc_ratio_hop_2}_{counter}_cora_test.pkl'), 'wb') as file:
-    pickle.dump(win_acc, file)
-
-with open(os.path.join(path, f'node_drop_large_winter_value_{group_trunc_ratio_hop_1}_{group_trunc_ratio_hop_2}_{counter}_cora_vali.pkl'), 'wb') as file:
-    pickle.dump(val_acc_list, file)
+    
+    # Iteratively drop nodes and evaluate
+    for j in tqdm(range(1, drop_num), desc='Dropping nodes', unit='node):
+        cur_player = node_list[j-1]
+        # print('cur_player: ',cur_player)
+        cur_node_list = node_list[:j]
+    
+        edge_mask = torch.ones(data.edge_index.shape[1], dtype=torch.bool, device=device)
+        for node in cur_node_list:
+            edge_mask[data.edge_index[0] == node] = False
+            edge_mask[data.edge_index[1] == node] = False
+    
+        edge_mask = edge_mask & indu_mask
+        data_copy = data.clone()
+        data_copy = data_copy.to(device)
+        data_copy.edge_index = data_copy.edge_index[:, edge_mask]
+    
+        model = SGCNet(num_features=dataset.num_features, num_classes=dataset.num_classes).to(device)
+        model.fit(data_copy)
+        test_acc = model.predict(test_data)
+        val_acc = model.predict_valid(val_data )
+        win_acc  +=[test_acc]
+        val_acc_list += [val_acc]
+            
+    # Save results
+    path = 'res/'
+    os.makedirs(path, exist_ok=True)
+    with open(os.path.join(path, f'node_drop_large_winter_value_{group_trunc_ratio_hop_1}_{group_trunc_ratio_hop_2}_{counter}_{dataset_name}_test.pkl'), 'wb') as file:
+        pickle.dump(win_acc, file)
+    
+    with open(os.path.join(path, f'node_drop_large_winter_value_{group_trunc_ratio_hop_1}_{group_trunc_ratio_hop_2}_{counter}_{dataset_name}_vali.pkl'), 'wb') as file:
+        pickle.dump(val_acc_list, file)
